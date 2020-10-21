9f40c77053d231c5a09e928c89620e35ee7ca51e
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2020-10-21 13:16:50 +0200

    Update style.js

--- style.js
+++ style.js
@@ -33,6 +33,7 @@
 				{ name: 'Énfasis-Bolder', element: 'span', attributes: { 'class': 'bck-enfasis-9'} },
 				{ name: 'Énfasis-10', element: 'span', attributes: { 'class': 'bck-enfasis-10'} },
 				{ name: 'Énfasis-11', element: 'span', attributes: { 'class': 'bck-enfasis-11'} },
+				{ name: 'Énfasis-12', element: 'span', attributes: { 'class': 'bck-enfasis-12'} },
 
 				{ name: 'Lista Ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-1' } },
 				{ name: 'Lista Ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
